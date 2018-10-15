773c8d4d327660f6f36f35be242b79c9fb21f4fd
Christian Dietrich <christian.dietrich@itemis.de>
2018-10-15 17:24:59 +0200

    [#608] fix maven build
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtend.maven.plugin/src/test/java/org/eclipse/xtend/maven/MavenVerifierUtil.java
+++ org.eclipse.xtend.maven.plugin/src/test/java/org/eclipse/xtend/maven/MavenVerifierUtil.java
@@ -22,17 +22,19 @@ public class MavenVerifierUtil {
 	public static Verifier newVerifier(String pathToTestProject) throws IOException, VerificationException {
 		File testDir = ResourceExtractor.simpleExtractResources(MavenVerifierUtil.class, pathToTestProject);
 		Verifier verifier = new Verifier(testDir.getAbsolutePath());
-		String localRepo = Paths.get("../.m2/repository/").toAbsolutePath().normalize().toString();
+		// String localRepo = Paths.get("../.m2/repository/").toAbsolutePath().normalize().toString();
+		String localRepo = System.getProperty("maven.repo.local");
 		System.out.println("gradle: " + System.getProperty("gradleMavenRepo"));
 		for (Object iterable_element : System.getProperties().keySet()) {
 			System.out.println(iterable_element + "=" + System.getProperty(iterable_element.toString()));
 		}
-		;
 
 		verifier.setSystemProperty("gradleMavenRepo", System.getProperty("gradleMavenRepo"));
 		verifier.setLocalRepo(localRepo);
 		verifier.setDebug(true);
 		// verifier.setMavenDebug(true);
+		verifier.addCliOption("-s");
+		verifier.addCliOption(System.getProperty("testSettingsXML"));
 		// verifier.setDebugJvm(true);
 		// verifier.setForkJvm(false);
 		return verifier;
